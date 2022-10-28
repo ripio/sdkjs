748ed03815b43cef7d2727d3866faa6c3fb1842d
Federico Aviles <federico.aviles@ripio.com>
2022-10-28 15:02:33 -0300

    update tests

--- src/__tests__/nft/storage/ResourceIpfs.test.ts
+++ src/__tests__/nft/storage/ResourceIpfs.test.ts
@@ -1,66 +1,22 @@
 import ResourceIpfs from '../../../nft/storage/ResourceIpfs'
-import errors from '../../../types/errors'
 
 describe('ResourceIpfs contructor', () => {
   it('Should create a new instance', () => {
     const resource = new ResourceIpfs([] as Uint8Array[])
     expect(resource).toBeInstanceOf(ResourceIpfs)
   })
-
-  it('Should throw error due not passing data', () => {
-    expect(() => {
-      new ResourceIpfs()
-    }).toThrowError(errors.IS_REQUIRED('data'))
-  })
 })
 
 describe('ResourceIpfs methods', () => {
-  it('Should retrieve an Uint8Array', () => {
-    const expected = new TextEncoder().encode('abc')
-    const data = [
-      new Uint8Array(new TextEncoder().encode('a')),
-      new Uint8Array(new TextEncoder().encode('b')),
-      new Uint8Array(new TextEncoder().encode('c'))
-    ]
-    const resource = new ResourceIpfs(data)
-    const result = resource.getBytesData()
-    expect(result).toStrictEqual(expected)
-  })
-
-  it('Should retrieve a string', () => {
-    const expected = 'abc'
-    const spyGetBytesData = jest
-      .spyOn(ResourceIpfs.prototype, 'getBytesData')
-      .mockReturnValueOnce(new TextEncoder().encode(expected))
-    const resource = new ResourceIpfs([] as Uint8Array[])
-    const result = resource.getStringData()
-    expect(result).toStrictEqual(expected)
-    expect(spyGetBytesData).toBeCalled()
-  })
-
-  it('Should retrieve a json', () => {
-    const expected = {
-      image: 'ipfs://fake-uri/image.png',
-      level: 0,
-      name: 'RD'
+  it('Should set the parsedData property', async () => {
+    const ipfsData = function* () {
+      yield new TextEncoder().encode('a')
+      yield new TextEncoder().encode('b')
+      yield new TextEncoder().encode('c')
     }
-    const spyGetStringData = jest
-      .spyOn(ResourceIpfs.prototype, 'getStringData')
-      .mockReturnValueOnce(JSON.stringify(expected))
-    const resource = new ResourceIpfs([] as Uint8Array[])
-    const result = resource.getJsonData()
-    expect(result).toStrictEqual(expected)
-    expect(spyGetStringData).toBeCalled()
-  })
-
-  it('Sould retrieve a base64 string', () => {
-    const expected = Buffer.from('hello!').toString('base64')
-    const spyGetBytesData = jest
-      .spyOn(ResourceIpfs.prototype, 'getBytesData')
-      .mockReturnValueOnce(new TextEncoder().encode('hello!'))
-    const resource = new ResourceIpfs([] as Uint8Array[])
-    const result = resource.getBase64Data()
-    expect(result).toStrictEqual(expected)
-    expect(spyGetBytesData).toBeCalled()
+    const resource = new ResourceIpfs(ipfsData())
+    expect(resource.parsedData).toBeUndefined()
+    await resource.parseData()
+    expect(resource.parsedData).toBeDefined()
   })
 })
--- src/__tests__/nft/storage/StorageIpfs.test.ts
+++ src/__tests__/nft/storage/StorageIpfs.test.ts
@@ -4,7 +4,6 @@ import {
 } from '../../../__mocks__/ipfs-http-client'
 import StorageIpfs from '../../../nft/storage/StorageIpfs'
 import ResourceIpfs from '../../../nft/storage/ResourceIpfs'
-import errors from '../../../types/errors'
 import * as ipfsUtils from '../../../utils/ipfs-utils'
 
 describe('StorageIpfs contructor', () => {
@@ -12,22 +11,11 @@ describe('StorageIpfs contructor', () => {
     const ipfs = new StorageIpfs('http://fake-ipfs-url:5001')
     expect(ipfs).toBeInstanceOf(StorageIpfs)
   })
-
-  it('Should throw error due not passing url', () => {
-    expect(() => {
-      new StorageIpfs()
-    }).toThrowError(errors.IS_REQUIRED('url'))
-  })
 })
 
 describe('StorageIpfs methods', () => {
   beforeEach(() => {
     __resetIPFSMocks()
-  })
-
-  it('Should throw error due not passing cidOrURI', async () => {
-    const ipfs = new StorageIpfs('http://fake-ipfs-url:5001')
-    await expect(ipfs.getData()).rejects.toThrow(errors.IS_REQUIRED('cidOrURI'))
   })
 
   it('Should retrieve a ResourceIpfs instance', async () => {
