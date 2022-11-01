ff93c4c6d4a2c3e4c55be0310257e3e012495c54
Agustin Mauras <agustin.mauras@ripio.com>
2022-11-01 10:56:55 -0300

    feat: delete extended managers

--- src/gaming/index.ts
+++
@@ -1,2 +0,0 @@
-export { ExtendedNFT721Manager } from './ExtendedNFT721Manager'
-export { ExtendedToken20Manager } from './ExtendedToken20Manager'
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './managers'
-export * from './gaming'
 export * from './utils'
 export * from './types'
 export * from './connectors'
