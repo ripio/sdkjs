ec7eba7cc62c23fcbe75a54d849f2e53ea91d576
Federico Aviles <federico.aviles@ripio.com>
2022-11-23 16:19:25 -0300

    add storageIpfs package

---
+++ packages/storageIpfs/src/__tests__/ResourceIpfs.test.ts
@@ -0,0 +1,22 @@
+import ResourceIpfs from '../ResourceIpfs'
+
+describe('ResourceIpfs contructor', () => {
+  it('Should create a new instance', () => {
+    const resource = new ResourceIpfs([] as Uint8Array[])
+    expect(resource).toBeInstanceOf(ResourceIpfs)
+  })
+})
+
+describe('ResourceIpfs methods', () => {
+  it('Should set the parsedData property', async () => {
+    const ipfsData = function* () {
+      yield new TextEncoder().encode('a')
+      yield new TextEncoder().encode('b')
+      yield new TextEncoder().encode('c')
+    }
+    const resource = new ResourceIpfs(ipfsData())
+    expect(resource.parsedData).toBeUndefined()
+    await resource.parseData()
+    expect(resource.parsedData).toBeDefined()
+  })
+})
