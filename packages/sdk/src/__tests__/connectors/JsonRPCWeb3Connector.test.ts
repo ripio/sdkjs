471716cca51e449bd3ee67d64309759bfb8cdd60
Federico Aviles <federico.aviles@ripio.com>
2023-03-03 10:57:40 -0300

    revert test

--- packages/sdk/src/__tests__/connectors/JsonRPCWeb3Connector.test.ts
+++ packages/sdk/src/__tests__/connectors/JsonRPCWeb3Connector.test.ts
@@ -102,7 +102,6 @@ describe('JsonRPCWeb3Connector activate method', () => {
     jest
       .spyOn(ethers.providers.JsonRpcProvider.prototype, 'getFeeData')
       .mockResolvedValueOnce({
-        lastBaseFeePerGas: null,
         maxFeePerGas: null,
         maxPriorityFeePerGas: null,
         gasPrice: BigNumber.from('1')
@@ -1000,7 +999,6 @@ describe('AbstractWeb3Connector detectLegacyChain method', () => {
     const mockGetFeeData = jest
       .spyOn(ethers.providers.JsonRpcProvider.prototype, 'getFeeData')
       .mockResolvedValueOnce({
-        lastBaseFeePerGas: null,
         maxFeePerGas: null,
         maxPriorityFeePerGas: null,
         gasPrice: BigNumber.from('1')
@@ -1018,7 +1016,6 @@ describe('AbstractWeb3Connector detectLegacyChain method', () => {
     const mockGetFeeData = jest
       .spyOn(ethers.providers.JsonRpcProvider.prototype, 'getFeeData')
       .mockResolvedValueOnce({
-        lastBaseFeePerGas: null,
         maxFeePerGas: BigNumber.from('1'),
         maxPriorityFeePerGas: BigNumber.from('1'),
         gasPrice: null
