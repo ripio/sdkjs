485cc5474b5f6c66cd3597463a1114b955078af3
Federico Aviles <federico.aviles@ripio.com>
2023-03-09 08:02:55 -0300

    fix: use named exports

--- packages/storageAws/src/index.ts
+++ packages/storageAws/src/index.ts
@@ -1,1 +1,3 @@
+// this default export will be deprecated in next versions
 export { default } from './StorageAws'
+export { default as StorageAws } from './StorageAws'
--- packages/storageHttp/src/index.ts
+++ packages/storageHttp/src/index.ts
@@ -1,1 +1,3 @@
+// this default export will be deprecated in next versions
 export { default } from './StorageHttp'
+export { default as StorageHttp } from './StorageHttp'
